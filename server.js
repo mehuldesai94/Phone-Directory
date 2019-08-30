3d3d5ee3243053a37292634e678f9c58152fce03
Mehul Desai <mmdesai@myseneca.ca>
2019-08-30 18:35:55 -0400

    basic app done

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const path = require("path");
-const app = express();
+const multer = require("multer");
+const bodyParser = require('body-parser');
 
+
+const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+    destination: "./public/photos",
+    filename: function(req, file, cb){
+        cb(null, Date.now()+ path.extname(file.originalname));
+    }
+})
+
+const upload = multer({storage:storage});
+
+app.get("/", (req, res)=>{
+  //  res.send("Mehul, You can do it...");
+  res.sendFile(path.join(__dirname, "/views/contact.html"));
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+})
+
+app.use((req, res)=>{
+    res.status(404).send("Page Not Found");
+})
+
+app.post("/register-user", upload.single("photo"), (req, res) => {
+    const formData = req.body;
+    const formFile = req.file;
+  
+    const dataReceived = "Your submission was received:<br/><br/>" +
+      "Your form data was:<br/>" + JSON.stringify(formData) + "<br/><br/>" +
+      "Your File data was:<br/>" + JSON.stringify(formFile) +
+      "<br/><p>This is the image you sent:<br/><img src='/photos/" + formFile.filename + "'/>";
+    res.send(dataReceived);
+  });
+  
 
 function onHttpStart(){
     console.log("Jay Swaminarayana");
     console.log("Express http server listing on: " + HTTP_PORT);
 }
-
-app.get("/", (req, res)=>{
-    res.send("Mehul, You can do it...");
-})
-
-app.get("/about", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/about.html"));
-})
 
 app.listen(HTTP_PORT, onHttpStart);
